f2f01ad9bcdcf4fdebd000f75f45080ca1652fc1
Aaron Wiggins <apw7177@gmail.com>
2019-11-24 21:25:38 -0500

    create click event to add ingredients dynamically. create basic price variable to keep track of burger prices eventually

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import classes from './buildControl.css';
-const buildControl = (props) => {
-    <div className={classes.buildControl}>
-        <div className={classes.label}>{props.label}</div>
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
-}
+);
 export default buildControl;
